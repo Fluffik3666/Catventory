17313277440e47a5395b6e6e490e222e89174567
Fluffik3666 <110257462+Fluffik3666@users.noreply.github.com>
2024-03-16 14:16:45 +0000

    added ui and qr code functionality

---
+++ Catventory/ShowAllScansView.swift
@@ -0,0 +1,18 @@
+//
+//  ShowAllScansView.swift
+//  Catventory
+//
+//  Created by Sasha Bagrov on 16/03/2024.
+//
+
+import SwiftUI
+
+struct ShowAllScansView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShowAllScansView()
+}
